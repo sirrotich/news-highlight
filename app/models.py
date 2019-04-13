dbd086d2a8110dd7ec7700774d83bd310d288481
sirrotich <rotichtitus12@gmail.com>
2019-04-13 22:42:19 +0300

    add the config options

---
+++ app/models.py
@@ -0,0 +1,19 @@
+class Source:
+
+    def __init__(self,id,name,description,url,category,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+
+class Articles:
+    
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+        self.author = author
+        self.title =title
+        self.description = description
+        self.url = url
+        self.urlToImage =urlToImage
+        self.publishedAt = publishedAt
--- config.py
+++ config.py
@@ -1,14 +1,19 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL ='https://newsapi.org/v2/everything?sources={}&pageSize={}&apiKey={}'
+    TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?language=en&pageSize={}&apiKey={}'
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
 
 class ProdConfig(Config):
     '''
     Production  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
@@ -18,9 +23,14 @@ class ProdConfig(Config):
 class DevConfig(Config):
     '''
     Development  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
 
     DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
